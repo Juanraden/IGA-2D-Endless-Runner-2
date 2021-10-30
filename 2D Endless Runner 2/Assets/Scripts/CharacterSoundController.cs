3749a3a23f2787bb1a1c9c40ff58f322d06fc63a
LAPTOP-M8OJ4LN0\Juanraden <juanraden@gmail.com>
2021-10-30 19:33:37 +0700

    1.0

---
+++ 2D Endless Runner 2/Assets/Scripts/CharacterSoundController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterSoundController : MonoBehaviour
+{
+    public AudioClip jump;
+    public AudioClip scoreHighlight;
+    public AudioClip coin;
+
+    private AudioSource audioPlayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioPlayer = GetComponent<AudioSource>();
+    }
+
+    public void PlayJump()
+    {
+        audioPlayer.PlayOneShot(jump);
+    }
+
+    public void PlayScoreHighlight()
+    {
+        audioPlayer.PlayOneShot(scoreHighlight);
+    }
+
+    public void PlayCoin()
+    {
+        audioPlayer.PlayOneShot(coin);
+    }
+}
---
+++ 2D Endless Runner 2/Assets/Scripts/Coin.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Coin : MonoBehaviour
+{
+    // Pemain yang akan bertambah skornya jika bola menyentuh dinding ini.
+    public ScoreController score;
+    public CharacterSoundController sound;
+    public Text text;
+
+    public float SpawnTime = 2f;
+    private float _spawnTime;
+
+    // Akan dipanggil ketika objek lain ber-collider (character) bersentuhan dengan koin.
+    [System.Obsolete]
+    void OnTriggerEnter2D(Collider2D anotherCollider)
+    {
+        // Jika objek tersebut bernama "Ball":
+        if (anotherCollider.name == "Main Character")
+        {
+            // Tambahkan skor ke pemain
+            score.IncreaseCurrentScore(5);
+
+            sound.PlayCoin();
+
+            text.gameObject.SetActive(true);
+            _spawnTime = SpawnTime;
+
+            foreach (Transform child in transform)
+            {
+                child.gameObject.SetActive(false);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        _spawnTime -= Time.unscaledDeltaTime;
+        if (_spawnTime <= 0f)
+        {
+            text.gameObject.SetActive(false);
+        }
+        else
+        {
+            text.CrossFadeAlpha(0f, 0.9f, false);
+            if (text.color.a == 0f)
+            {
+                text.gameObject.SetActive(false);
+            }
+        }
+    }
+}
